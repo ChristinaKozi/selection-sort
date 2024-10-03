ccd5c01bf0c11feb42be8dd315545cea0fb9aef5
Christina Kozanian <christinakozanian@gmail.com>
2024-10-02 20:03:26 -0700

    solution in go

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+)
+
+func selectionSort(arr []int) []int {
+	sorted := []int{}
+
+	for len(arr) > 0 {
+		lowestInt := arr[0]
+		for _, val := range arr {
+			if val < lowestInt {
+				lowestInt = val
+			}
+		}
+		sorted = append(sorted, lowestInt)
+
+		for i, val := range arr {
+			if val == lowestInt {
+				arr = append(arr[:i], arr[i+1:]...)
+				break
+			}
+		}
+	}
+
+	return sorted
+}
+
+func main() {
+	fmt.Println(selectionSort([]int{5, 7, 3, 9}))
+}
